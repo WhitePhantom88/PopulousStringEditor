d0756e93450c94c94176399b75e331d10a076037
Khickman <whitestriker900@yahoo.com>
2017-03-13 23:08:38 -0400

    Correct inconsistent use of tabs and spaces.

--- PopulousStringEditor/Views/StringComparisonView.xaml.cs
+++ PopulousStringEditor/Views/StringComparisonView.xaml.cs
@@ -7,16 +7,16 @@ namespace PopulousStringEditor.Views
     /// </summary>
     public partial class StringComparisonView : UserControl
     {
-		/// <summary>Gets or sets the visibility of the referenced strings.</summary>
+        /// <summary>Gets or sets the visibility of the referenced strings.</summary>
         public System.Windows.Visibility ReferenceStringsVisiblity
         {
             get { return ReferenceStringsColumn.Visibility; }
             set { ReferenceStringsColumn.Visibility = value; }
         }
 
-		/// <summary>
-		/// Default constructor.
-		/// </summary>
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public StringComparisonView()
         {
             InitializeComponent();
